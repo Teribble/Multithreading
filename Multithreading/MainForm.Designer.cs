27afc6e35d8860da4321c105fa7beaa2ce5f6d43
Евгений Гергелиджиу <Euphor1avegas@gmail.com>
2022-09-15 21:45:31 +0300

    Добавьте файлы проекта.

---
+++ Multithreading/MainForm.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Multithreading
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(33, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Генерация числа в диапазоне: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(147, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "От: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(156, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "До: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "2";
+            this.textBox1.TextChanged += new System.EventHandler(this.OnTextChangedTextBox);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(200, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "Max";
+            this.textBox2.TextChanged += new System.EventHandler(this.OnTextChangedTextBox);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.errorLabel);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(312, 185);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Задание 1:";
+            this.groupBox1.TextChanged += new System.EventHandler(this.OnTextChangedTextBox);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(116, 2);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(111, 16);
+            this.errorLabel.TabIndex = 8;
+            this.errorLabel.Text = "Ошибка в вводе";
+            this.errorLabel.Visible = false;
+            this.errorLabel.VisibleChanged += new System.EventHandler(this.OnVisibleChange);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(8, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(261, 9);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Если не указана верхняя граница, генерация до завершения программы";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 32);
+            this.button1.TabIndex = 7;
+            this.button1.TabStop = false;
+            this.button1.Text = "Запустить поток";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnButton1Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(8, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 9);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Если не указана нижняя граница, старт с 2";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 203);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(312, 91);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Задание 2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(7, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(207, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Генерация чисел Фибоначчи - ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(84, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 32);
+            this.button2.TabIndex = 10;
+            this.button2.TabStop = false;
+            this.button2.Text = "Запустить поток";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnClickButton2);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(214, 33);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label8.Size = new System.Drawing.Size(14, 16);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "0";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox3.Location = new System.Drawing.Point(330, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(563, 143);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Задание 3 - 4";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 32);
+            this.button3.TabIndex = 10;
+            this.button3.TabStop = false;
+            this.button3.Text = "Завершить первое задание";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.OnButton3Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(284, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(264, 32);
+            this.button4.TabIndex = 11;
+            this.button4.TabStop = false;
+            this.button4.Text = "Завершить второе задание";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.OnButton4Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(284, 59);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(264, 32);
+            this.button5.TabIndex = 13;
+            this.button5.TabStop = false;
+            this.button5.Text = "Остановить второе задание";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(14, 59);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(264, 32);
+            this.button6.TabIndex = 12;
+            this.button6.TabStop = false;
+            this.button6.Text = "Остановить первое задание";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.OnButton6Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(284, 97);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(264, 32);
+            this.button7.TabIndex = 15;
+            this.button7.TabStop = false;
+            this.button7.Text = "Возобновить второе задание";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(14, 97);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(264, 32);
+            this.button8.TabIndex = 14;
+            this.button8.TabStop = false;
+            this.button8.Text = "Возобновить первое задание";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(906, 588);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OnClosedForm);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Multithreading/MainForm.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Multithreading
+{
+    public partial class MainForm : Form
+    {
+        private List<Thread> Threads;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Threads = new List<Thread>();
+        }
+
+        private void OnButton1Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(Task1);
+
+            thread.Name = nameof(Task1);
+
+            Threads.Add(thread);
+
+            thread.Start(label2);
+        }
+
+        public void Task1(object obj)
+        {
+            int min = 0, max = 0;
+
+            Examination(textBox1.Text, textBox2.Text, out min, out max);
+
+            var label = (Label)obj;
+
+            for (int i = min; i <= max; i++)
+            {
+                label.Invoke(new Action(() => label.Text = i.ToString()));
+
+                Thread.Sleep(200);
+            }
+        }
+
+        private void OnClosedForm(object sender, FormClosedEventArgs e)
+        {
+            foreach (var thread in Threads)
+            {
+                thread.Abort();
+            }
+        }
+
+        private void OnTextChangedTextBox(object sender, EventArgs e)
+        {
+            var bufferLubel = (TextBox)sender;
+
+            if (Int32.TryParse(bufferLubel.Text, out int a) || bufferLubel.Text == string.Empty)
+                textBox1.Invoke(new Action(() => errorLabel.Visible = false));
+            else
+                textBox1.Invoke(new Action(() => errorLabel.Visible = true));
+        }
+
+        private void OnVisibleChange(object sender, EventArgs e)
+        {
+            if (errorLabel.Visible)
+                button1.Enabled = false;
+            else
+                button1.Enabled = true;
+        }
+
+        public void Task2(object obj)
+        {
+            if(Int32.TryParse(textBox2.Text, out int max))
+            {
+                max = Int32.Parse(textBox2.Text);
+            }
+            else
+            {
+                max = int.MaxValue;
+            }
+
+            Label label = (Label)obj;
+
+            int j = 1;
+
+            for (int i = 1; i <= max; i += j)
+            {
+                label.Invoke(new Action(() => label.Text = i.ToString()));
+
+                j = i - j;
+
+                Thread.Sleep(200);
+            }
+        }
+
+        private void OnClickButton2(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(Task2);
+
+            thread.Name = nameof(Task2);
+
+            Threads.Add(thread);
+
+            thread.Start(label8);
+        }
+
+        private void Examination(string str1, string str2, out int a, out int b)
+        {
+            if (str1 == "2")
+                a = 2;
+            else
+                Int32.TryParse(textBox1.Text, out a);
+
+            if (str2 == "Max")
+                b = int.MaxValue;
+            else
+                Int32.TryParse(textBox2.Text, out b);
+
+            if (a > b)
+                (a, b) = (b, a);
+        }
+
+        private void OnButton3Click(object sender, EventArgs e)
+        {
+            Threads.Where(n => n.Name == nameof(Task1)).ToList().ForEach(n=>n.Abort());
+        }
+
+        private void OnButton4Click(object sender, EventArgs e)
+        {
+            Threads.Where(n => n.Name == nameof(Task2)).ToList().ForEach(n => n.Abort());
+        }
+
+        private void OnButton6Click(object sender, EventArgs e)
+        {
+            Threads.Where(n => n.Name == nameof(Task2)).ToList().ForEach(n => n.Join());
+
+            Threads.Where(n => n.Name == nameof(Task2)).ToList().ForEach(n => n.Interrupt());
+        }
+    }
+
+}
